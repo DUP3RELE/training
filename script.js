5fa9956f2db2d240f08afbc23765282f5004a8de
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-10-09 18:11:26 +0200

    first commit

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ var addTwoPromises = async function(promise1, promise2) {
     
     return value1 + value2
 };
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    
+    return value1 + value2
+};
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    
+    return value1 + value2
+};
+varv
+gi
