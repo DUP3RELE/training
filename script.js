4dde67217e2dc5fb36de5feaa184374bbf68731c
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-10-25 14:47:17 +0200

    exercise

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const expect = (val) => {
-    const throwError = (errorStr) => {throw new Error(errorStr)};
+function gcdOfStrings(str1, str2) {
+    if (str1 + str2 !== str2 + str1) {
+        return '';
+    }
+
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const commonLength = gcd(str1.length, str2.length);
     
-    return {
-        toBe:    (val2) => val2 === val || throwError('Not Equal'),
-        notToBe: (val2) => val2 !== val || throwError('Equal'),
-    };
-};
+    return str1.slice(0, commonLength);
+}
+
+
+const str1 = "ABCABC";
+const str2 = "ABC";
+const result = gcdOfStrings(str1, str2);
+console.log(result);
