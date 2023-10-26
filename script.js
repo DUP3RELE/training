3b6d8b71caf1713df6e3e065fe0145ec6ba2fdce
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-10-26 23:20:16 +0200

    test

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function gcdOfStrings(str1, str2) {
     
     return str1.slice(0, commonLength);
 }
+function gcdOfStrings(str1, str2) {
+    if (str1 + str2 !== str2 + str1) {
+        return '';
+    }
+
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const commonLength = gcd(str1.length, str2.length);
+    
+    return str1.slice(0, commonLength);
+}
 
 
 const str1 = "ABCABC";
