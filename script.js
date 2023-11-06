642c96bb38967936699f2366485658bedc7c9a47
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-06 17:42:53 +0100

    origin

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
 
-var compose = function(functions) {
-  function composeHelper(f, g) {
-  return (x) => f(g(x));
-}
- let composedFunction = functions[functions.length - 1];
-for (let i = functions.length - 2; i >= 0; i--) {
-  composedFunction = composeHelper(functions[i], composedFunction);
-}
+// var compose = function(functions) {
+//   function composeHelper(f, g) {
+//   return (x) => f(g(x));
+// }
+//  let composedFunction = functions[functions.length - 1];
+// for (let i = functions.length - 2; i >= 0; i--) {
+//   composedFunction = composeHelper(functions[i], composedFunction);
+// }
 
-return composedFunction;
-}
+// return composedFunction;
+// }
+
+var compose = function(functions) {
+  return x => functions.reduceRight((acc,f)=>f(acc),x)
+};
+
+ console.log(function);
