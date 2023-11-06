77bbf2aef285f651ae6bea03cd65d796b352721c
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-06 17:20:55 +0100

    training

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function memoize(fn) {
-    const cache = {};
-  
-    return function (...args) {
-      const key = JSON.stringify(args);
-      if (key in cache) {
-        return cache[key];
-      } else {
-        const result = fn(...args);
-        cache[key] = result;
-        return result;
-      }
-    };
-  }
+functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+
+var compose = function(functions) {
+  function composeHelper(f, g) {
+  return (x) => f(g(x));
+}
+ let composedFunction = functions[functions.length - 1];
+for (let i = functions.length - 2; i >= 0; i--) {
+  composedFunction = composeHelper(functions[i], composedFunction);
+}
+
+return composedFunction;
+}
