d6310e180fecdc3ee9b16d4b78c9fc86e856c710
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-07 10:44:27 +0100

    async f

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+// functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+
+// // var compose = function(functions) {
+// //   function composeHelper(f, g) {
+// //   return (x) => f(g(x));
+// // }
+// //  let composedFunction = functions[functions.length - 1];
+// // for (let i = functions.length - 2; i >= 0; i--) {
+// //   composedFunction = composeHelper(functions[i], composedFunction);
+// // }
+
+// // return composedFunction;
+// // }
 
 // var compose = function(functions) {
-//   function composeHelper(f, g) {
-//   return (x) => f(g(x));
-// }
-//  let composedFunction = functions[functions.length - 1];
-// for (let i = functions.length - 2; i >= 0; i--) {
-//   composedFunction = composeHelper(functions[i], composedFunction);
-// }
+//   return x => functions.reduceRight((acc,f)=>f(acc),x)
+// };
 
-// return composedFunction;
-// }
 
-var compose = function(functions) {
-  return x => functions.reduceRight((acc,f)=>f(acc),x)
-};
 
- console.log(function);
+
+//  console.log(function);
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((resolve) => {
+  setTimeout(resolve, millis);
+});
+}
+
+
+/** 
+* let t = Date.now()
+* sleep(100).then(() => console.log(Date.now() - t)) // 100
+*/
