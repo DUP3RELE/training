2b79fe5014af486e251f6f42aa7416065acbd7ee
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-14 22:30:15 +0100

    2727. Is Object Empty

--- script.js
+++ script.js
@@ -28,3 +28,11 @@ var reverseVowels = function(s) {
   }
   return chars.join('');
 };
+
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0 ? true : false;
+};
