b047e4cb03e68b3f63e7d00a0e4d8f4e71df96cc
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-14 11:09:58 +0100

    345. Reverse Vowels of a String

--- script.js
+++ script.js
@@ -1,132 +1,30 @@
 /**
- * @param {Function} fn
- * @param {Array} args
- * @param {number} t
- * @return {Function}
+ * @param {string} s
+ * @return {string}
  */
-var cancellable = function(fn, args, t) {
-  let timeoutId;
-
-const cancelFn = () => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-    timeoutId = null;
+var reverseVowels = function(s) {
+  const chars = s.split('');
+  
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+  
+  let left = 0;
+  let right = chars.length - 1;
+  
+  while (left < right) {
+      while (left < right && !vowels.has(chars[left])) {
+          left++;
+      }
+      while (left < right && !vowels.has(chars[right])) {
+          right--;
+      }
+      if (left < right) {
+          const temp = chars[left];
+          chars[left] = chars[right];
+          chars[right] = temp;
+          
+          left++;
+          right--;
+      }
   }
+  return chars.join('');
 };
-
-const executeFn = () => {
-  if (timeoutId) {
-    fn(...args);
-    timeoutId = null;
-  }
-};
-
-timeoutId = setTimeout(executeFn, t);
-
-return cancelFn;
-};
-
-/**
-*  const result = []
-*
-*  const fn = (x) => x * 5
-*  const args = [2], t = 20, cancelT = 50
-*
-*  const start = performance.now() 
-*
-*  const log = (...argsArr) => {
-*      const diff = Math.floor(performance.now() - start);
-*      result.push({"time": diff, "returned": fn(...argsArr)})
-*  }
-*       
-*  const cancel = cancellable(log, args, t);
-*
-*  const maxT = Math.max(t, cancelT)
-*           
-*  setTimeout(() => {
-*     cancel()
-*  }, cancelT)
-*
-*  setTimeout(() => {
-*     console.log(result) // [{"time":20,"returned":10}]
-*  }, maxT + 15)
-*/
-
-/**
- * @param {number[]} candies
- * @param {number} extraCandies
- * @return {boolean[]}
- */
-var kidsWithCandies = function(candies, extraCandies) {
-  const maxCandies = Math.max(...candies)
-     const result = candies.map(candy => candy + extraCandies >= maxCandies)
-         return result
-};
-
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
-var canPlaceFlowers = function(flowerbed, n) {
-    let count = 0;
-    for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] === 0) {
-            const prevEmpty = i === 0 || flowerbed[i - 1] === 0;
-            const nextEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
-
-            if (prevEmpty && nextEmpty) {
-                flowerbed[i] = 1;
-                count++;
-                i++;
-            }
-        }
-    }
-
-    return count >= n;
-};
-
-/**
- * @param {Function} fn
- * @param {Array} args
- * @param {number} t
- * @return {Function}
- */
-var cancellable = function(fn, args, t) {
-  fn(...args);
-   let timer = setInterval(()=> fn(...args),t);
-
-   let cancelFn = () => clearInterval(timer)
-   return cancelFn;
-};
-
-/**
-*  const result = []
-*
-*  const fn = (x) => x * 2
-*  const args = [4], t = 35, cancelT = 190
-*
-*  const start = performance.now()
-*
-*  const log = (...argsArr) => {
-*      const diff = Math.floor(performance.now() - start)
-*      result.push({"time": diff, "returned": fn(...argsArr)})
-*  }
-*       
-*  const cancel = cancellable(log, args, t);
-*
-*  setTimeout(() => {
-*     cancel()
-*  }, cancelT)
-*   
-*  setTimeout(() => {
-*    console.log(result)  // [
-*                         //      {"time":0,"returned":8},
-*                         //      {"time":35,"returned":8},
-*                         //      {"time":70,"returned":8},           
-*                         //      {"time":105,"returned":8},
-*                         //      {"time":140,"returned":8},
-*                         //      {"time":175,"returned":8}
-*                         //  ]
-*  }, cancelT + t + 15)    
-*/
