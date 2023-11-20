c2330a7071c5fcf4bfec2175f6c59642a7c77d93
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-11-20 16:36:01 +0100

    392. Is Subsequence

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
-// // /**
-// //  * @param {Promise} promise1
-// //  * @param {Promise} promise2
-// //  * @return {Promise}
-// //  */
-// // var addTwoPromises = async function(promise1, promise2) {
-// //     let p1r = await promise1
-// //     let p2r =  await promise2
-// //     let promise3 = p1r + p2r
-// //     return promise3;
-// //  };
+// // // /**
+// // //  * @param {Promise} promise1
+// // //  * @param {Promise} promise2
+// // //  * @return {Promise}
+// // //  */
+// // // var addTwoPromises = async function(promise1, promise2) {
+// // //     let p1r = await promise1
+// // //     let p2r =  await promise2
+// // //     let promise3 = p1r + p2r
+// // //     return promise3;
+// // //  };
  
-// //  /**
-// //   * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
-// //   *   .then(console.log); // 4
-// //   */
+// // //  /**
+// // //   * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+// // //   *   .then(console.log); // 4
+// // //   */
+
+// // /**
+// //  * @param {Array} arr
+// //  * @param {number} size
+// //  * @return {Array}
+// //  */
+// // var chunk = function(arr, size) {
+// //     let result = []   
+// //     for(let i = 0; i < arr.length; i += size) {
+// //         result.push(arr.slice(i, i + size))
+// //     }
+// //     return result
+// // };
 
 // /**
-//  * @param {Array} arr
-//  * @param {number} size
-//  * @return {Array}
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
 //  */
-// var chunk = function(arr, size) {
-//     let result = []   
-//     for(let i = 0; i < arr.length; i += size) {
-//         result.push(arr.slice(i, i + size))
+// var moveZeroes = function(nums) {
+//     let index = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let temp = nums[index];
+//       nums[index] = nums[i];
+//       nums[i] = temp;
+//       index++;
 //     }
-//     return result
+//   }
 // };
-
 /**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
  */
-var moveZeroes = function(nums) {
-    let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      let temp = nums[index];
-      nums[index] = nums[i];
-      nums[i] = temp;
-      index++;
-    }
+var isSubsequence = function(s, t) {
+  let sIndex = 0;
+let tIndex = 0;
+
+while (sIndex < s.length && tIndex < t.length) {
+  if (s[sIndex] === t[tIndex]) {
+    sIndex++;
   }
+  tIndex++;
+}
+
+return sIndex === s.length;
 };
